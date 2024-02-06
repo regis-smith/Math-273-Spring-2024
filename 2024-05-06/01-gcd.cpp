6eb343ad63355fdda47f896456013c0b275b2e09
Regis Smith <rsmith@whistlin.com>
2024-02-06 11:34:34 -0800

    Euclid's greatest common divisor.

---
+++ 2024-05-06/01-gcd.cpp
@@ -0,0 +1,30 @@
+// Euclid's greatest common divisor function
+// Input: Positive integers M and N
+// Result: GCD of M and N
+// Step 1: If M=N the stop, for the GCD is M.
+// Step 2: Replace the larger of M and N with
+//         their (positive) difference.
+// Step 3: Go to Step 1.
+
+#include <iostream>
+
+int gcd(int m, int n)
+{
+  // Step 1
+  if (m == n) return m;
+  // Step 2
+  if (m > n) {
+    m = m - n;
+  }
+  else {
+    n = n - m;
+  }
+  // Step 3
+  return gcd(m,n); // recursively to Step 1
+}
+
+int main()
+{
+  std::cout << "The GCD of 250 and 350 is "
+	    << gcd(250,350) << ".\n";
+}
