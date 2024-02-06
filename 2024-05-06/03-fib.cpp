9eb06baff6324afe0c1f8dca9f4e06744cda242a
Regis Smith <rsmith@whistlin.com>
2024-02-06 11:49:49 -0800

    Fibonacci function.  Very slow.

---
+++ 2024-05-06/03-fib.cpp
@@ -0,0 +1,17 @@
+// Fibonacci function -- impractical recursion
+// Computing fib(46) will be noticeably slower
+#include <iostream>
+
+int fib(int n)
+{
+  if (n == 0 or n == 1) return n;
+  else return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+  std::cout << "About to compute fib(20).\n"
+	    << "fib(20) = " << fib(20) << ".\n";
+  std::cout << "About to compute fib(46)." << std::endl
+	    << "fib(46) = " << fib(46) << ".\n";
+}
