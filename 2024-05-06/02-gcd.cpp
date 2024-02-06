04156eeea0887e78a8530fe15d23d25b70274613
Regis Smith <rsmith@whistlin.com>
2024-02-06 11:40:11 -0800

    Euclid's GCD -- iterative version.

---
+++ 2024-05-06/02-gcd.cpp
@@ -0,0 +1,23 @@
+// Euclid's greatest common divisor function
+// Iterative version
+#include <iostream>
+
+int gcd(int m, int n)
+{
+  while (m != n) { // Step 1: check if m equals n.
+    // Step 2: Replace larger of m,n with their difference.
+    if (m > n) {
+      m = m - n;
+    }
+    else {
+      n = n - m;
+    }
+  } // Step 3: Go to Step 1--beginning of while statement
+  return m;
+}
+
+int main()
+{
+  std::cout << "The GCD of 250 and 350 is "
+	    << gcd(250,350) << ".\n";
+}
