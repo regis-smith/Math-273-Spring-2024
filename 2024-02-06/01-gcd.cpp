a8e8cc360414f4121d5189864f84bc74ec202d38
Regis Smith <rsmith@whistlin.com>
2024-02-08 11:30:47 -0800

    Euclid's greatest common divisor.

--- 2024-02-06/01-gcd.cpp
+++ 2024-02-06/01-gcd.cpp
@@ -1,6 +1,6 @@
 // Euclid's greatest common divisor function
 // Input: Positive integers M and N
-// Result: GCD of M and N
+// Result: The greatest common divisor of M and N
 // Step 1: If M=N the stop, for the GCD is M.
 // Step 2: Replace the larger of M and N with
 //         their (positive) difference.
