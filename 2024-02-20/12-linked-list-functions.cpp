516fd30ad277f81504122651729c67686e0b16fe
Regis Smith <rsmith@whistlin.com>
2024-02-20 10:32:11 -0800

    Liked list insertion.

--- 2024-02-20/12-linked-list-functions.cpp
+++ 2024-02-20/12-linked-list-functions.cpp
@@ -21,12 +21,25 @@ void list_insert_before_front(node<K>*& list, const K& key)
   list = new_front;
 }
 
+template <typename K>
+void list_remove_front(node<K>*& list)
+{
+  if (list == nullptr) return;
+  auto old_front = list;
+  list = list -> next;
+  delete old_front;
+}
+
 int main()
 {
   node<char>* my_list = nullptr; // empty list
   list_insert_before_front(my_list, 'a');
   list_insert_before_front(my_list, 'b');
   list_insert_before_front(my_list, 'c');
+
+  list_print(my_list);
+
+  list_remove_front(my_list);
 
   list_print(my_list);
 }
