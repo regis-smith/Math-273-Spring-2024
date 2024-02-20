b1ff1256da44222d829487393191ce6ba1ff5343
Regis Smith <rsmith@whistlin.com>
2024-02-20 10:13:07 -0800

    Linked list first examples.

---
+++ 2024-02-20/12-linked-list-functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+template <typename T>
+struct node {
+  T key;
+  node* next = nullptr;
+};
+
+template <typename T>
+void list_print(node<T>* front) {
+  for (auto i = front; i != nullptr; i = i -> next) {
+    std::cout << i -> key << '\t';
+  }
+  std::cout << std::endl;
+}
+
+template <typename K>
+void list_insert_before_front(node<K>*& list, const K& key)
+{
+  auto new_front = new node<K> {key, list};
+  list = new_front;
+}
+
+int main()
+{
+  node<char>* my_list = nullptr; // empty list
+  list_insert_before_front(my_list, 'a');
+  list_insert_before_front(my_list, 'b');
+  list_insert_before_front(my_list, 'c');
+
+  list_print(my_list);
+}
+
+  
